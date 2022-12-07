95bb3c173f423455a0c92edbc0c7d31392ae6eac
Lab <seoye6618@gmail.com>
2022-12-08 02:22:13 +0900

    [FEAT] add new functions and class in TaxiDAO

--- src/main/java/com/drive/TaxiDAO.java
+++ src/main/java/com/drive/TaxiDAO.java
@@ -2,8 +2,11 @@ package com.drive;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 
 @Repository
@@ -36,4 +39,25 @@ public class TaxiDAO {
         return template.update(DRIVE_UPDATE, new Object[]{vo.getUsername(), vo.getPhonenumber(), vo.getDeparture(), vo.getArrival(), vo.getGrade(), vo.getReserveDate(), vo.isWheelchair(), vo.getId()});
     }
 
+}
+
+/*this class is for showing data list*/
+class DriveRowMapper implements RowMapper<TaxiVO> {
+    @Override
+    public TaxiVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+        TaxiVO vo = new TaxiVO();
+        vo.setId(rs.getInt("id"));
+        vo.setUsername(rs.getString("username"));
+        vo.setPhonenumber(rs.getString("phonenumber"));
+        vo.setDeparture(rs.getString("departure"));
+        vo.setArrival(rs.getString("arrival"));
+        vo.setGrade(rs.getString("grade"));
+        vo.setReserveDate(rs.getDate("reserveDate"));
+        vo.setWheelchair(rs.getBoolean("wheelchair"));
+        return vo;
+    }
+
+//    public TaxiVO getDrive(int id){
+//        return
+//    }
 }
