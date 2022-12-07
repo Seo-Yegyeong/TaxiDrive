99e53d18d8a82269b457b281bd753dbb4881fb58
Lab <seoye6618@gmail.com>
2022-12-08 02:03:21 +0900

    [CHORE] add encoding setting in web.xml

--- src/main/java/com/drive/TaxiDAO.java
+++ src/main/java/com/drive/TaxiDAO.java
@@ -1,16 +1,24 @@
 package com.drive;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
+@Repository
 public class TaxiDAO {
+
+    @Autowired
     private JdbcTemplate template;
 
     public void setTemplate(JdbcTemplate template) {
         this.template = template;
     }
 
+    //VO variables : id, username, phonenumber, departure, arrival, grade, reserveDate, wheelchair;
     private final String DRIVE_INSERT = "insert into DRIVE (username, phonenumber, departure, arrival, grade, reserveDate, wheelchair) values (?,?,?,?,?,?,?)";
-    private final String DRIVE_UPDATE = "update DRIVE set name=? number=? departure=? arrival=? level=? reserveDate=? wheelchair=? where id=?";
+    private final String DRIVE_UPDATE = "update DRIVE set username=? phonenumber=? departure=? arrival=? grade=? reserveDate=? wheelchair=? where id=?";
     private final String DRIVE_DELETE = "delete from DRIVE where id=?";
 
 
@@ -19,8 +27,13 @@ public class TaxiDAO {
                 Object[]{vo.getUsername(), vo.getPhonenumber(), vo.getDeparture(), vo.getArrival(), vo.getGrade(), vo.getReserveDate(), vo.isWheelchair()});
     }
 
-    public int deleteDRIVE(int id) {
-        return 1;
+    public int deleteDRIVE(int id)
+    {
+        return template.update(DRIVE_DELETE, new Object[]{id});
+    }
+
+    public int updateDRIVE(TaxiVO vo){
+        return template.update(DRIVE_UPDATE, new Object[]{vo.getUsername(), vo.getPhonenumber(), vo.getDeparture(), vo.getArrival(), vo.getGrade(), vo.getReserveDate(), vo.isWheelchair(), vo.getId()});
     }
 
 }
